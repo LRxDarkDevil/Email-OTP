919aab4a75f8d01d1711b728b117f70a5ee87ff6
Dark Dare Devil <71424575+LRxDarkDevil@users.noreply.github.com>
2023-02-21 21:49:18 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+  let mail = "RECEIVES_EMAIL_ADDRESS_HERE"
+// Getting random code
+  let code = Math.floor(Math
+    .random() * (999999 - 100000 + 1)) + 100000;
+//Importing HTML for email
+  let html = fs.readFileSync("email.txt","utf8")
+  html = html.replace("{replace}", code);
+//Creating Transport
+  let transporter = nodemailer.createTransport({
+    host: "HOST_HERE",//cp7.mywebsitebox.com
+    auth: {
+      user: "EMAIL_ADDRESS_HERE", 
+      pass: "PASSWORD_HERE",
+    }
+  });
+  var mailOptions = {
+    from: 'SENDER_EMAIL_ADDRESS_HERE',
+    to: mail,
+    subject: 'SUBJECT_HERE',
+    html: html
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } 
+  });
